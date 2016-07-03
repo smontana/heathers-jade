51d73b5d0f2c1050de12a38ada73d407df3dda34
Steve Montana <stephen.a.montana@gmail.com>
2016-07-03 10:57:24 -0500

    Went with pug & gulp

--- app/background.js
+++ app/background.js
@@ -1,28 +1,23 @@
 import { app, Menu } from 'electron'
 import createWindow from './lib/window'
-
-// TODO: implement gulp build
-// import env from './env'
+import env from './env'
 
 var mainWindow
 
 app.on('ready', function () {
   var mainWindow = createWindow('main', {
     width: 1000,
-    height: 600,
-    titleBarStyle: 'hidden-inset',
-    autoHideMenuBar: true
+    height: 600
+    // TODO: Import below for windows
+    // ,titleBarStyle: 'hidden-inset',
+    // autoHideMenuBar: true
   })
 
-  // mainWindow.loadURL('file://' + __dirname + '/app.html')
-  mainWindow.loadURL('file://' + __dirname + '/app.jade')
+  mainWindow.loadURL('file://' + __dirname + '/app.html')
 
-  // if (env.name !== 'production') {
-  //   mainWindow.openDevTools()
-  // }
-
-  mainWindow.openDevTools()
-
+  if (env.name !== 'production') {
+    mainWindow.openDevTools()
+  }
 })
 
 app.on('window-all-closed', function () {
--- build/app.js
+++ build/app.js
@@ -1,11 +1,17 @@
-import os from 'os' // native node.js module
-import { remote } from 'electron' // native electron module
-import jetpack from 'fs-jetpack' // module loaded from npm
+(function () {'use strict';
 
-import env from './env'
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var os = require('os');
+var electron = require('electron');
+var jetpack = _interopDefault(require('fs-jetpack'));
+
+// The variables have been written to `env.json` by the build process.
+var env = jetpack.cwd(__dirname).read('env.json', 'json');
+
 console.log('Loaded environment variables:', env)
 
-var app = remote.app
+var app = electron.remote.app
 var appDir = jetpack.cwd(app.getAppPath())
 
 console.log('The author of this app is:', appDir.read('package.json', 'json').author)
@@ -16,3 +22,5 @@ document.addEventListener('DOMContentLoaded', function () {
   // document.getElementById('env-name').innerHTML = env.name
   console.log('DOM Content Loaded!')
 })
+}());
+//# sourceMappingURL=app.js.map
